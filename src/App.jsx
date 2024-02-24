2ffa03f0cf0ba73fca7c8116f10fd119664479d2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-02-24 12:06:18 +0600

    created taskReducer Function

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,64 @@ import { useState } from 'react';
 import AddTask from './components/AddTask';
 import TaskList from './components/Tasklist';
 
-export default function TaskApp() {
-  const [tasks, setTasks] = useState(initialTasks);
 
-  function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
-  }
-
-  function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        }
+      ]
+    }
+    case 'changed': {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
         } else {
           return t;
         }
       })
+    }
+    case 'deleted': {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error('Unknown Action : ', action.type);
+    }
+  }
+}
+
+export default function TaskApp() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  function handleAddTask(text) {
+    dispatch(
+      {
+        type: 'added'
+        id: nextId++,
+        text: text,
+        done: false,
+      },
     );
   }
 
+  function handleChangeTask(task) {
+    // action object
+    dispatch({
+      type: 'changed',
+      task,
+    })
+  }
+
   function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+    dispatch({
+      type: 'deleted',
+      taskId,
+    })
   }
 
   return (
