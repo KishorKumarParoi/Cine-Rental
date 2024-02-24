ca86f0c24cd224d785a4bc7701402a055e69d53c
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-02-24 10:45:30 +0600

    learned preserving and destroying state

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-function Panel({ title, children, isActive, onShow }) {
-
+export default function Scoreboard() {
+  const [isPlayerA, setIsPlayerA] = useState(true);
   return (
-    <>
-      <h1 className="text-3xl">{title}</h1>
-      {
-        isActive ? <p>{children}</p> :
-          <button className="bg-yellow-500 rounded-lg p-2" onClick={onShow} >Show</button>
-      }
-    </>
-  )
-
+    <div>
+      {isPlayerA ? (
+        <Counter key="Taylor" person="Taylor" />
+      ) : (
+        <Counter key="Sarah" person="Sarah" />
+      )}
+      <button onClick={() => {
+        setIsPlayerA(!isPlayerA);
+      }}>
+        Next player!
+      </button>
+    </div>
+  );
 }
 
-export default function Accordion() {
-  const [activeIndex, setActiveIndex] = useState(0);
+function Counter({ person }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
 
   return (
-    <div className="m-4 p-4">
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel title="About" isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}>
-        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel title="Etymology" isActive={activeIndex === 1} onShow={() => setActiveIndex(1)}>
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-      </Panel>
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{person}'s score: {score}</h1>
+      <button onClick={() => setScore(score + 1)}>
+        Add one
+      </button>
     </div>
   );
 }
