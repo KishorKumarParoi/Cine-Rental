5bafdb30d6d68abcdf1592388f5b70488936ff56
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-02-26 08:26:45 +0600

    splitted out components

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,23 @@
-import { useImmerReducer } from 'use-immer';
-import AddTask from './components/AddTask';
-import TaskList from './components/Tasklist';
-import tasksReducer from './components/taskReducer';
+import AllPosts from './components/AllPosts';
+import Heading from './components/Heading';
+import Post from './components/Post';
+import Section from './components/Section';
 
-export default function TaskApp() {
-  const [tasks, dispatch] = useImmerReducer(tasksReducer, initialTasks);
-
-  function handleAddTask(text) {
-    dispatch(
-      {
-        type: 'added',
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    );
-  }
-
-  function handleChangeTask(task) {
-    // action object
-    dispatch({
-      type: 'changed',
-      task,
-    })
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: 'deleted',
-      id: taskId,
-    })
-  }
-
+export default function ProfilePage() {
   return (
-    <>
-      <h1>Prague itinerary</h1>
-      <AddTask onAddTask={handleAddTask} />
-      <TaskList
-        tasks={tasks}
-        onChangeTask={handleChangeTask}
-        onDeleteTask={handleDeleteTask}
+    <Section>
+      <Heading>My Profile</Heading>
+      <Post
+        title="Hello traveller!"
+        body="Read about my adventures."
       />
-    </>
+      <AllPosts />
+    </Section>
   );
 }
 
-let nextId = 3;
-const initialTasks = [
-  { id: 0, text: 'Visit Kafka Museum', done: true },
-  { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false },
-];
+
+
+
+
+
---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { LevelContext } from "../contexts/LevelContext";
+
+export default function Section({ isFancy, children }) {
+    const level = useContext(LevelContext);
+
+    return (
+        <section className={"section " + (isFancy ? 'fancy' : '')}>
+            <LevelContext.Provider value={level + 1}>
+                {children}
+            </LevelContext.Provider>
+        </section>
+    );
+}
