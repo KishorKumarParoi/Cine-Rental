0be4b4a71a736cb853e275541db5082aa16fde00
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-03-05 18:58:44 +0600

    created movieContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
 import Footer from "./Footer"
 import Header from "./Header"
 import Main from "./Page"
+import { MovieContext } from "./contexts"
 
 export default function App() {
+  const [cartData, setCartData] = useState([]);
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <MovieContext.Provider value={{ cartData, setCartData }}>
+        <Header />
+        <Main />
+        <Footer />
+      </MovieContext.Provider>
     </>
   )
 }
