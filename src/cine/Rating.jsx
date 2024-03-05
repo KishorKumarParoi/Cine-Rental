1020845fc515906776395a67bb64aa437eafb588
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-03-05 12:06:44 +0600

    deleted index.js, created MovieList, made two components MovieCard and Rating

---
+++ src/cine/Rating.jsx
@@ -0,0 +1,15 @@
+import Star from '../assets/star.svg';
+
+export default function Rating({ value }) {
+    const stars = Array(value).fill(Star);
+
+    return (
+        <>
+            {
+                stars.map((star, index) => (
+                    <img src={Star} key={index} width="14" height="14" alt="star" />
+                ))
+            }
+        </>
+    )
+}
